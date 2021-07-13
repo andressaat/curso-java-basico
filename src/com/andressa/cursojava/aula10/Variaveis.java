f1ff9c546c18a3ed5fb3fbd1ba69ec942997b0a1
Andressa <andressa.a.tenorio88@gmail.com>
2021-07-12 23:38:45 -0300

    Atualisação ate a Aula 10

---
+++ src/com/andressa/cursojava/aula10/Variaveis.java
@@ -0,0 +1,21 @@
+package com.andressa.cursojava.aula10;
+
+public class Variaveis {
+    public static void main(String[] args){
+
+        int idade = 33;
+        String nome = "Dessa";
+        String nomeDoMeuCachorro = "Padora";
+        String ano2014 = "2014";
+
+        idade = 25;
+
+        System.out.println("Idade = " + idade);
+        System.out.println("Nome = " + nome);
+
+
+
+
+
+    }
+}
